3a4c1879400ff584fd5fe75690c7f17ab0951a89
Louis <louis.zk143@gmail.com>
2026-09-19 09:56:50 +0200

    Release v1.2.0

--- enlidea/settings/base.py
+++ enlidea/settings/base.py
@@ -83,7 +83,7 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     "TITLE": "Enlidea API",
     "DESCRIPTION": "Autonomous Multi-Agent Research Hub API",
-    "VERSION": "1.1.0",
+    "VERSION": "1.2.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "COMPONENT_SPLIT_REQUEST": True,
     "ENUM_NAME_OVERRIDES": {
